c2f74ca6a7b0f9b4c99c1313b6e46745487f027c
Gd2010 <128714689+dacoder123456m@users.noreply.github.com>
2023-10-31 17:30:08 -0500

    Update global.js

--- global.js
+++ global.js
@@ -46,7 +46,9 @@ function displayCookieEnablePopup() {
         document.body.removeChild(popup);
     }
 }
-// check if they have the signed in cookie and if they do then it will not do anything if they do not have it it will redirect them to the signin screen
+
+// Check if they have the signed-in cookie, and if they do, it will not do anything.
+// If they do not have it, it will redirect them to the sign-in screen.
 function checkAuthState() {
     const signedInCookie = document.cookie.includes("signed_in=true");
     if (!signedInCookie) {
@@ -59,6 +61,7 @@ function checkAuthState() {
 document.addEventListener("DOMContentLoaded", function() {
     checkAuthState();
 });
+
 // Create the loading logo image
 const img = document.createElement("img");
 img.src = "watermark.png";
@@ -72,72 +75,81 @@ img.style.borderRadius = "50%";
 img.classList.add("rotate"); // Add the rotate class
 
 document.addEventListener('DOMContentLoaded', () => {
-  document.body.appendChild(img);
+    document.body.appendChild(img);
 
-  // Wait for the page to fully load
-  window.addEventListener('load', () => {
-    // Add an artificial delay of .4 seconds to run this code below
-    setTimeout(() => {
-      document.body.removeChild(img);
+    // Wait for the page to fully load
+    window.addEventListener('load', () => {
+        // Add an artificial delay of .4 seconds to run this code below
+        setTimeout(() => {
+            document.body.removeChild(img);
 
-      // Retrieve and apply the saved page title, background image, and favicon
-      const savedTitle = localStorage.getItem("pageTitle");
-      if (savedTitle) {
-        document.title = savedTitle;
-      }
+            // Retrieve and apply the saved page title and background image
+            const savedTitle = localStorage.getItem("pageTitle");
+            if (savedTitle) {
+                document.title = savedTitle;
+            } else {
+                // If no saved title is found, set a preset title
+                document.title = "WuscoChat";
+            }
 
-      const background = localStorage.getItem('background');
-      if (background) {
-        document.body.style.backgroundImage = background;
-      }
+            const background = localStorage.getItem('background');
+            if (background) {
+                document.body.style.backgroundImage = background;
+            }
 
-      const urlf = localStorage.getItem("favicon");
-      const favicon = document.querySelector('link[rel="shortcut icon"]') || document.createElement('link');
-      favicon.type = 'image/x-icon';
-      favicon.rel = 'shortcut icon';
-      favicon.href = urlf;
-      document.head.appendChild(favicon);
+            // Retrieve the preset favicon and apply it
+            const presetFavicon = 'favicon.ico'; // Replace with your actual preset favicon path
+            const currentFavicon = document.querySelector('link[rel="shortcut icon"]');
+            if (currentFavicon && currentFavicon.getAttribute('href') !== presetFavicon) {
+                currentFavicon.setAttribute('href', presetFavicon);
+            } else if (!currentFavicon) {
+                const favicon = document.createElement('link');
+                favicon.type = 'image/x-icon';
+                favicon.rel = 'shortcut icon';
+                favicon.href = presetFavicon;
+                document.head.appendChild(favicon);
+            }
 
-      // Load hamburger menu
-      const hamburgerMenu = document.querySelector('.hamburger-menu');
-      const hamburgerMenuIcon = document.querySelector('.hamburger-menu__icon');
-      const hamburgerMenuLinks = document.querySelector('.hamburger-menu__links');
-      const menuLinks = [
-        { title: 'Settings', url: 'settings.html' },
-        { title: 'Live Chat', url: 'chat.html' }
-      ];
+            // Load hamburger menu
+            const hamburgerMenu = document.querySelector('.hamburger-menu');
+            const hamburgerMenuIcon = document.querySelector('.hamburger-menu__icon');
+            const hamburgerMenuLinks = document.querySelector('.hamburger-menu__links');
+            const menuLinks = [
+                { title: 'Settings', url: 'settings.html' },
+                { title: 'Live Chat', url: 'chat.html' }
+            ];
 
-      const generateMenuLinks = () => {
-        const menuLinksHTML = menuLinks.map(link => `<a href="${link.url}" class="menu-link">${link.title}</a>`).join('');
-        hamburgerMenuLinks.innerHTML = menuLinksHTML;
-      };
+            const generateMenuLinks = () => {
+                const menuLinksHTML = menuLinks.map(link => `<a href="${link.url}" class="menu-link">${link.title}</a>`).join('');
+                hamburgerMenuLinks.innerHTML = menuLinksHTML;
+            };
 
-      hamburgerMenuIcon.addEventListener('click', () => {
-        hamburgerMenu.classList.toggle('active');
-        hamburgerMenuLinks.style.height = hamburgerMenu.classList.contains('active') ? 'auto' : '0';
-      });
+            hamburgerMenuIcon.addEventListener('click', () => {
+                hamburgerMenu.classList.toggle('active');
+                hamburgerMenuLinks.style.height = hamburgerMenu.classList.contains('active') ? 'auto' : '0';
+            });
 
-      generateMenuLinks();
+            generateMenuLinks();
 
-      menuLinks.forEach(link => {
-        const anchor = hamburgerMenuLinks.querySelector(`[href="${link.url}"]`);
-        anchor.addEventListener('click', () => {
-          const href = anchor.getAttribute('href');
-          window.location.href = href;
-        });
-      });
+            menuLinks.forEach(link => {
+                const anchor = hamburgerMenuLinks.querySelector(`[href="${link.url}"]`);
+                anchor.addEventListener('click', () => {
+                    const href = anchor.getAttribute('href');
+                    window.location.href = href;
+                });
+            });
 
-      // Check for password protection and prompt the user if necessary
-      const password = localStorage.getItem('password') || '';
-      const passwordActive = localStorage.getItem('passwordActive') === 'true';
+            // Check for password protection and prompt the user if necessary
+            const password = localStorage.getItem('password') || '';
+            const passwordActive = localStorage.getItem('passwordActive') === 'true';
 
-      if (password && passwordActive) {
-        const input = prompt('Enter password:');
-        if (input !== password) {
-          alert('Incorrect password!');
-          window.location.href = 'https://www.youtube.com/watch?v=o-YBDTqX_ZU';
-        }
-      }
-    }, 400); // Wait for .4 seconds (400 milliseconds) before executing this code above
-  });
+            if (password && passwordActive) {
+                const input = prompt('Enter password:');
+                if (input !== password) {
+                    alert('Incorrect password!');
+                    window.location.href = 'https://www.youtube.com/watch?v=o-YBDTqX_ZU';
+                }
+            }
+        }, 400); // Wait for .4 seconds (400 milliseconds) before executing this code above
+    });
 });
