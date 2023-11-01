8ec09aa644cd9de7dc8576256fb8e476a56471f8
InfinityLoop <127774659+InfinityLoopGames@users.noreply.github.com>
2023-11-01 11:52:01 -0500

    Update chat.js

--- chat.js
+++ chat.js
@@ -130,7 +130,7 @@ const firebaseConfig = {
     leftSidebar.setAttribute('id', 'left-sidebar');
     leftSidebar.setAttribute('class', 'meme-container');      
     // add what you want here enzo
-    leftSidebar.innerHTML = '<iframe src="https://wuscoadvertisement-485877.netlify.app/" width="200px" height="500px"></iframe>'; 
+    leftSidebar.innerHTML = '<iframe src="https://wuscoadvertisement-485877.netlify.app/" width="200px" height="700px"></iframe>'; 
     leftSidebar.classList.add('left-sidebar'); 
     document.body.append(leftSidebar);
   }
