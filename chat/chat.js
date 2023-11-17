97cd8719d2d4a19136d2dbbcc10ab67ab0ace75d
InfinityLoop <127774659+InfinityLoopGames@users.noreply.github.com>
2023-11-17 14:22:57 -0600

    Update chat.js

--- chat/chat.js
+++ chat/chat.js
@@ -148,6 +148,10 @@ const firebaseConfig = {
       <li>Rule 1: No spamming or flooding the chat.</li>
       <li>Rule 2: No text art or NSFW/NSFW links.</li>
       <li>Rule 3: No busting its NNN, you must pass it.</li>
+      <button class="panic" onclick="location.href='https://classroom.google.com'">
+        Panic Button (WIP)
+      </button>
+
           `;
 
     rightSidebar.append(rules);
